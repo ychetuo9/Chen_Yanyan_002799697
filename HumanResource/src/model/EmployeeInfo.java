38d7cbca3a9117e36d7b88be276db1dbdaff1960
ychetuo25 <survivor19yyc@outlook.com>
2022-09-26 19:29:27 -0400

    Added HumanSource project

---
+++ HumanResource/src/model/EmployeeInfo.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author yanyanchen
+ */
+public class EmployeeInfo {
+    private String name;
+    private int employeeId;
+    private int age;
+    private String gender;
+    private String startDate;
+    private int level;
+    private String teamInfo;
+    private String positionTitle;
+    private String contactInfo;
+    private String phoneNumber;
+    private String emailAddress;
+    private String Photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(String Photo) {
+        this.Photo = Photo;
+    }
+    
+}
---
+++ HumanResource/src/model/EmployeeInfoHistory.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author yanyanchen
+ */
+public class EmployeeInfoHistory {
+    private ArrayList<EmployeeInfo> history;
+    
+    public EmployeeInfoHistory(){
+        this.history = new ArrayList<EmployeeInfo>();
+    }
+
+    public ArrayList<EmployeeInfo> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<EmployeeInfo> history) {
+        this.history = history;
+    }
+    
+    public EmployeeInfo addNewEmployee(){
+        EmployeeInfo newEmployee = new EmployeeInfo();
+        history.add(newEmployee);
+        return newEmployee;
+    }
+}
